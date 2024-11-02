1e1197cb4e65c91fe3337142b7f66efcd0299097
Khachatur <khachatur.martirosyan.96@gmail.com>
2024-11-02 15:05:13 +0400

    add craco config and chakra ui v3

---
+++ client/src/components/ProviderComponent.js
@@ -0,0 +1,9 @@
+import { Provider } from "@/components/ui/provider";
+
+const ProviderComponent = ({ children }) => (
+    <Provider>
+        {children}
+    </Provider>
+);
+
+export default ProviderComponent;
