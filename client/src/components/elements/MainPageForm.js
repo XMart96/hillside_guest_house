ad1e57dc98fa132d00522699bddab9140a65eb4f
Khachatur <khachatur.martirosyan.96@gmail.com>
2025-03-01 16:01:10 +0400

    end main page form

---
+++ client/src/components/elements/MainPageForm.js
@@ -0,0 +1,118 @@
+import { NumberInputRoot, NumberInputField } from '@/components/ui/number-input';
+import { Field, Flex, Input } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
+import { Controller, useForm } from 'react-hook-form';
+import Btn from './Btn';
+
+const MainPageForm = () => {
+    const { t } = useTranslation(['mainPageForm']);
+    const { register, handleSubmit, formState: { errors }, reset, control } = useForm({ 
+        mode: 'onChange', 
+        defaultValues: {
+            adult: 2
+        }
+    }); 
+    
+    const onSubmit = handleSubmit(data => {
+        console.log(data);
+        reset();
+    });
+    
+    return (
+        <form onSubmit={onSubmit} style={{ width: '100%' }}>
+            <Flex 
+                gap={{ base: '5', md: '8' }} 
+                align={{ base: 'center', md: 'end' }} 
+                direction={{ base: 'column', md: 'row' }}
+            >
+                <Field.Root invalid={!!errors.checkIn}>
+                    <Field.Label>
+                        {t('checkIn')}
+                        <Field.RequiredIndicator />
+                    </Field.Label>
+                    <Input 
+                        type='date'
+                        name='checkIn'
+                        _focusVisible={{ borderColor: 'grn', outlineColor: 'grn' }}
+                        borderColor="gry"
+                        _placeholder={{ color: 'gry' }}
+                        min={new Date().toISOString().split('T')[0]}
+                        {...register('checkIn', { 
+                            required: t('requiredCheckIn'),
+                            pattern: {
+                                value: /^\d{4}-\d{2}-\d{2}$/,
+                                message: t('errorCheckIn')
+                            } 
+                        })}
+                    />
+                    <Field.ErrorText>{errors.checkIn?.message}</Field.ErrorText>
+                </Field.Root>
+
+                <Field.Root invalid={!!errors.checkOut}>
+                    <Field.Label>
+                        {t('checkOut')}
+                        <Field.RequiredIndicator />
+                    </Field.Label>
+                    <Input
+                        type='date' 
+                        name='checkOut'
+                        _focusVisible={{ borderColor: 'grn', outlineColor: 'grn' }}
+                        borderColor="gry"
+                        _placeholder={{ color: 'gry' }}
+                        {...register('checkOut', { 
+                            required: t('requiredCheckOut'),
+                            pattern: {
+                                value: /^\d{4}-\d{2}-\d{2}$/,
+                                message: t('errorCheckOut')
+                            } 
+                        })}
+                    />
+                    <Field.ErrorText>{errors.checkOut?.message}</Field.ErrorText>
+                </Field.Root>
+
+                <Field.Root invalid={!!errors.adult}>
+                    <Field.Label>
+                        {t('adult')}
+                        <Field.RequiredIndicator />
+                    </Field.Label>
+                    <Controller
+                        control={control}
+                        name="adult"
+                        rules={{
+                            required: t('requiredAdult'),
+                            min: {
+                                value: 1,
+                                message: t('errorMinAdult')
+                            },
+                            max: {
+                                value: 8,
+                                message: t('errorMaxAdult')
+                            },
+                        }}
+                        render={({ field }) => (
+                            <NumberInputRoot
+                                minW={{ md: '80px'}}
+                                {...field} 
+                                value={field.value}
+                                onValueChange={({value}) => field.onChange(value)}
+                            >
+                                <NumberInputField
+                                    _focusVisible={{ borderColor: 'grn', outlineColor: 'grn' }}
+                                    borderColor="gry"
+                                    _placeholder={{ color: 'gry' }}
+                                />
+                            </NumberInputRoot>
+                        )}
+                    />
+                    <Field.ErrorText>{errors.adult?.message}</Field.ErrorText>
+                </Field.Root>
+
+                <Btn type="submit">
+                    {t('button')}
+                </Btn>
+            </Flex>
+        </form>
+    );
+}
+
+export default MainPageForm;
--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -1,119 +1,32 @@
-import { Flex, Input } from '@chakra-ui/react';
-import { NumberInputRoot, NumberInputField } from '@/components/ui/number-input';
-import { Field } from '@/components/ui/field';
+import { Flex} from '@chakra-ui/react';
 import image from '../../assets/room4/9-min.jpg';
-import { useTranslation } from 'react-i18next';
-import { Controller, useForm } from 'react-hook-form';
-import  Btn from '../elements/Btn';
+import MainPageForm from '../elements/MainPageForm';
 
-const HomePage = () => {
-    const { t } = useTranslation(['mainPageForm']);
-    const { register, handleSubmit, control, formState: { errors } } = useForm();
-    const onSubmit = handleSubmit(data => console.log(data));
-
-    return (
-        <Flex 
-            position="relative"
-            flex='1'
-            bgImage={`url(${image})`}
-            bgSize='cover'
-            backgroundPosition='center'
+const HomePage = () => (
+    <Flex 
+        position="relative"
+        flex='1'
+        bgImage={`url(${image})`}
+        bgSize='cover'
+        backgroundPosition='center'
+    >
+        
+        <Flex
+            position="absolute"
+            bottom="10"
+            left="50%"
+            transform="translateX(-50%)"
+            bg="white"
+            color="gry"
+            rounded="2xl"
+            p="6"
+            justify="center"
+            w={{ base: '95%', sm: '70%', md: 'auto' }}
+            shadow="md"
         >
-            
-            <Flex
-                position="absolute"
-                bottom="10"
-                left="50%"
-                transform="translateX(-50%)"
-                bg="white"
-                color="gry"
-                rounded="2xl"
-                p="6"
-                justify="center"
-                w={{ base: '95%', sm: '70%', md: 'auto' }}
-                shadow="md"
-            >
-                <form onSubmit={onSubmit} style={{ width: '100%' }}>
-                    <Flex gap={{ base: '5', md: '8' }} align={{ base: 'center', md: 'end' }} direction={{ base: 'column', md: 'row' }}>
-                        <Field
-                            label={t('checkIn')}
-                            w={{ base: '100%', md: '150px' }}
-                            invalid={!!errors.checkIn}
-                            errorText={errors.checkIn?.message}
-                        >
-                            <Input
-                                type="date"
-                                name="checkIn"
-                                _focusVisible={{ borderColor: 'grn', outlineColor: 'grn' }}
-                                borderColor="gry"
-                                _placeholder={{ color: 'gry' }}
-                                {...register('checkIn', { required: t('errorCheckIn') })}
-                            />
-                        </Field>
-                        <Field
-                            label={t('checkOut')}
-                            w={{ base: '100%', md: '150px' }}
-                            invalid={!!errors.checkOut}
-                            errorText={errors.checkOut?.message}
-                        >
-                            <Input
-                                type="date"
-                                name="checkOut"
-                                _focusVisible={{ borderColor: 'grn', outlineColor: 'grn' }}
-                                borderColor="gry"
-                                _placeholder={{ color: 'gry' }}
-                                {...register('checkOut', { required: t('errorCheckOut') })}
-                            />
-                        </Field>
-                        <Flex gap="5" w='100%'>
-                            <Field
-                                label={t('adult')}
-                                w={{base: '100%', md: "80px"}}
-                                invalid={!!errors.adult}
-                                errorText={errors.adult?.message}
-                            >
-                                <Controller
-                                    name="adult"
-                                    control={control}
-                                    rules={{ required: t('errorAdult'), min: 1, max: 8 }}
-                                    defaultValue={2}
-                                    render={({ field }) => (
-                                        <NumberInputRoot {...field} min={1} max={8} value={field.value} onValueChange={({value}) => field.onChange(value)}>
-                                            <NumberInputField
-                                                _focusVisible={{ borderColor: 'grn', outlineColor: 'grn' }}
-                                                borderColor="gry"
-                                                _placeholder={{ color: 'gry' }}
-                                            />
-                                        </NumberInputRoot>
-                                    )}
-                                />
-                            </Field>
-                            <Field label={t('children')} w={{base: '100%', md: "80px"}}>
-                                <Controller
-                                    name="children"
-                                    control={control}
-                                    defaultValue={0}
-                                    rules={{ min: 0, max: 8 }}
-                                    render={({ field }) => (
-                                        <NumberInputRoot {...field} min={0} max={8} value={field.value} onValueChange={({value}) => field.onChange(value)}>
-                                            <NumberInputField
-                                                _focusVisible={{ borderColor: 'grn', outlineColor: 'grn' }}
-                                                borderColor="gry"
-                                                _placeholder={{ color: 'gry' }}
-                                            />
-                                        </NumberInputRoot>
-                                    )}
-                                />
-                            </Field>
-                        </Flex>
-                        <Btn type="submit">
-                            {t('button')}
-                        </Btn>
-                    </Flex>
-                </form>
-            </Flex>
+            <MainPageForm />
         </Flex>
-    );
-};
+    </Flex>
+);
 
 export default HomePage;
